772b9c89ccf36dffcc33b084e3f2b4458ee6957d
Toby Inkster <tobyink@cpan.org>
2012-05-29 22:51:25 +0000

    Import of TOBYINK/Task-BeLike-TOBYINK-2012.150 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-TOBYINK
    gitpan-cpan-version:      2012.150
    gitpan-cpan-path:         TOBYINK/Task-BeLike-TOBYINK-2012.150.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/TOBYINK.pm
@@ -0,0 +1,56 @@
+package Task::BeLike::TOBYINK;
+
+use 5.010;
+
+BEGIN {
+	$Task::BeLike::TOBYINK::AUTHORITY = 'cpan:TOBYINK';
+	$Task::BeLike::TOBYINK::VERSION   = '2012.150';
+}
+
+__PACKAGE__
+__END__
+
+=head1 NAME
+
+Task::BeLike::TOBYINK - be like TOBYINK and use these stuffz
+
+=head1 DESCRIPTION
+
+Task installs the following modules:
+
+=over
+
+=item * Task::BeLike::TOBYINK::Data
+
+=item * Task::BeLike::TOBYINK::Moose
+
+=item * Task::BeLike::TOBYINK::Plack
+
+=item * Task::BeLike::TOBYINK::RDF
+
+=item * Task::BeLike::TOBYINK::Utils
+
+=item * Task::BeLike::TOBYINK::Web
+
+=back
+
+=head1 SEE ALSO
+
+L<Acme::About::TOBYINK>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
